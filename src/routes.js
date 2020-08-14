ed36ea447f827ab0437e3be2160bf975098f7866
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-08-14 05:10:46 -0300

    schedule index

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +21,8 @@ routes.put('/users', authMiddleware, UserController.update);
 routes.post('/sessions', SessionController.store);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/schedule', authMiddleware, ScheduleController.index);
 
 routes.get('/appointments', authMiddleware, AppointmentController.index);
 routes.post('/appointments', authMiddleware, AppointmentController.store);
