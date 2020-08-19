4a9148e9052018627ddaf985b1fb770e5e2c3c64
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-08-19 20:15:40 -0300

    fix mail config

---
+++ src/config/mail.js
@@ -0,0 +1,13 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '59cfaa3c66bab6',
+    pass: '18959af47f7999',
+  },
+
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
